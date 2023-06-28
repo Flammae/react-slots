34eec9b34b398de2e44c4300d2b5940edb455c45
beqa <b.meqvabishvili159@gmail.com>
2023-06-28 15:22:07 +0400

    Removed playground files from git history

--- playground/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { MyComponent } from '../';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<MyComponent />);
